ab769a09953165e5b033ecfacaa86e2c3f047bce
Stephen Torres <joreltorres99@gmail.com>
2022-11-04 15:21:19 +0800

    created CRUD functions

--- app/controllers/bookcontroller.js
+++ app/controllers/bookcontroller.js
@@ -34,17 +34,78 @@ exports.findAll = (req, res) => {
     })
 };
 exports.findOne = (req, res) => {
-    
+    Book.findById(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+              res.status(404).send({
+                message: `Not found Book with id ${req.params.id}.`
+              });
+            } else {
+              res.status(500).send({
+                message: "Error retrieving Book with id " + req.params.id
+              });
+            }
+        } else res.send(data);
+    });
 };
 exports.findAllPublished = (req, res) => {
-    
+    Book.getAllPublished((err, data) => {
+        if (err)
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while retrieving books."
+          });
+        else res.send(data);
+      });
 };
 exports.update = (req, res) => {
-    
+    if (!req.body) {
+        res.status(400).send({
+          message: "Content can not be empty!"
+        });
+      }
+      console.log(req.body);
+
+      Book.updateById(
+        req.params.id,
+        new Book(req.body),
+        (err, data) => {
+          if (err) {
+            if (err.kind === "not_found") {
+              res.status(404).send({
+                message: `Not found Book with id ${req.params.id}.`
+              });
+            } else {
+              res.status(500).send({
+                message: "Error updating Book with id " + req.params.id
+              });
+            }
+          } else res.send(data);
+        }
+      );
 };
 exports.delete = (req, res) => {
-    
+    Book.remove(req.params.id, (err, data) => {
+        if (err) {
+          if (err.kind === "not_found") {
+            res.status(404).send({
+              message: `Not found Book with id ${req.params.id}.`
+            });
+          } else {
+            res.status(500).send({
+              message: "Could not delete Book with id " + req.params.id
+            });
+          }
+        } else res.send({ message: `Book was deleted successfully!` });
+      });
 };
 exports.deleteAll = (req, res) => {
-    
+    Book.removeAll((err, data) => {
+        if (err)
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while removing all books."
+          });
+        else res.send({ message: `All Books were deleted successfully!` });
+      });
 };
